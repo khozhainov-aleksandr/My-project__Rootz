0ef439fc9b925244bde1bb0296f50051b82722bd
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-07-27 10:34:12 +0300

    Prepare NewsSection

---
+++ layout/NewsSection/NewsSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NewsSectionProps } from "./NewsSection.props";
+// import styles from './NewsSection.module.scss';
+// import classNames from "classnames";
+
+export const NewsSection: React.FC<NewsSectionProps> = (): JSX.Element => {
+  return ( 
+    <div></div>
+  );
+};
