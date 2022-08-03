6520c030e6a5a57f5aa51dc48eb8bc1ddca8d821
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-03 15:26:47 +0300

    Fix all erorr in Carusel section

--- layout/CarouselCard/CarouselCard.props.ts
+++ layout/CarouselCard/CarouselCard.props.ts
@@ -1,7 +1,3 @@
-// import { DetailedHTMLProps, HTMLAttributes } from "react";
-
-// export interface CarouselCardProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
-
 export interface CardContent {
   id: string;
   title: string;
--- layout/CarouselCard/CarouselCard.tsx
+++ layout/CarouselCard/CarouselCard.tsx
@@ -1,28 +1,76 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { CardContent } from "./CarouselCard.props";
-import { Carousel } from "../../components/Carousel";
 import { Card } from "../../components/Card";
+import { Picture } from "../../components/Picture";
 import styles from './CarouselCard.module.scss';
 
 
 export const CarouselCard = (): JSX.Element => {
+  const [pages, setPages] = useState<CardContent[]>([]);
+  const [offset, setOffset] = useState<number>(0);
+
+  const PAGE_WIDTH = 992;
+
+  useEffect(() => {
+    setPages(cardContent);
+  }, []);
+
+  function leftArrowClick() {
+    setOffset((currentOffset) => {
+      const newOffset = currentOffset + PAGE_WIDTH;
+      return Math.min(newOffset, 0);
+    });
+  }
+
+  function rightArrowClick() {
+    setOffset((currentOffset) => {
+      const newOffset = currentOffset - PAGE_WIDTH;
+      const maxOffset = -(PAGE_WIDTH * (pages.length - 1));
+      return Math.max(newOffset, maxOffset);
+    });
+  }
+
   return ( 
     <div className={styles.CarouselCard}>
       <div className="container">
-        <Carousel>
-          {cardContent.map((card, i) => {
-            const cardNumber = i + 1;
+        <div className={styles.mainWrapper}>
+          <Picture 
+            className={styles.arrow}
+            format="svg"
+            imgName="arrow-left"
+            altDes="Arrow Left"
+            onClick={leftArrowClick}
+          />
 
-            return (
-              <Card
-                key={card.id}
-                classCard={styles[`card__${cardNumber}`]}
-                titleChildren={card.title}
-                descChildren={card.description}
-              />
-            );
-          })}
-        </Carousel>
+          <div className={styles.visibleArea}>
+            <div 
+              className={styles.allCardsWrapper}
+              style={{transform: `translateX(${offset}px)`}}
+            >
+              {cardContent.map((card, i) => {
+                const cardNumber = i + 1;
+
+                return (
+                  <Card
+                    key={card.id}
+                    classCard={styles[`card__${cardNumber}`]}
+                    titleChildren={card.title}
+                    descChildren={card.description}
+                  />
+                );
+              })}
+            </div>
+          </div>
+
+          <Picture 
+            className={styles.arrow}
+            format="svg"
+            imgName="arrow-right"
+            altDes="Arrow Right"
+            onClick={rightArrowClick}
+          />
+        </div>
       </div>
     </div>
   );
